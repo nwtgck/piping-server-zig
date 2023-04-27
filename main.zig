042e96f825b8d20cae5db4b4008dccff8d8c0a2e
Ryo Ota <nwtgck@nwtgck.org>
2023-04-27 22:37:24 +0900

    inject HTTP port as command line arguments

--- main.zig
+++ main.zig
@@ -4,11 +4,21 @@ const PipingServer = @import("./PipingServer.zig");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+
+    const http_port: u16 = blk: {
+        var args = std.process.args();
+        _ = args.next();
+        if (args.next()) |port_str| {
+            break :blk try std.fmt.parseInt(u16, port_str, 10);
+        }
+        break :blk 8080;
+    };
+
     const max_header_size = 8192;
     var server = std.http.Server.init(allocator, .{ .reuse_address = true });
     defer server.deinit();
 
-    const address = try std.net.Address.parseIp("0.0.0.0", 8080);
+    const address = try std.net.Address.parseIp("0.0.0.0", http_port);
     std.debug.print("Listening on {} ...\n", .{address});
     try server.listen(address);
 
